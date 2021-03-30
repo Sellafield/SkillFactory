44e90385a4b662a17be5f3768cadd9299837cd1f
Viktor Dolzhukov <viktor.dolzhukov@intravision.ru>
2021-03-30 16:01:11 +0300

    v5

--- Project_0/game.py
+++ Project_0/game.py
@@ -26,6 +26,25 @@ def game_core_v2(number):
       predict -= 1
   return (count)  # выход из цикла, если угадали
 
+def int_r(num):
+  num = int(num + (0.5 if num > 0 else -0.5))
+  return num
+
+def game_core_v3(number):
+  '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+     Функция принимает загаданное число и возвращает число попыток'''
+  count = 1
+
+  predict = np.random.randint(1, 101)
+  while number != predict:
+    count += 1
+    if number > predict:
+      predict += int_r((number - predict) / 2)
+    elif number < predict:
+      predict -= int_r((predict - number) / 2)
+
+  return (count)  # выход из цикла, если угадали
+
 def score_game(game_core):
   '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
   count_ls = []
@@ -38,4 +57,4 @@ def score_game(game_core):
   return (score)
 
 # запускаем
-score_game(game_core_v2)
+score_game(game_core_v3)
